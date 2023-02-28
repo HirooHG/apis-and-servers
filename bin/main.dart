13c68dac60df7b7e4f195765465f30490912f698
HirooHG <hugo.golliet@etu.univ-smb.fr>
2023-02-28 21:04:21 +0100

    any ipv4

--- bin/main.dart
+++ bin/main.dart
@@ -3,11 +3,12 @@ import 'dart:io';
 import 'package:multigamewebsocketsdart/requesthandler.dart';
 
 Future<void> main() async {
-  var server = await HttpServer.bind(InternetAddress.loopbackIPv4, 3402);
+  var server = await HttpServer.bind(InternetAddress.anyIPv4, 3402);
   print("Listening on ws://${server.address.address}:${server.port}");
 
   await for (HttpRequest request in server) {
     var handler = await RequestHandler.init(req: request);
+    print("yay");
     if(handler == null) {
       print("cannot handle request");
     }
