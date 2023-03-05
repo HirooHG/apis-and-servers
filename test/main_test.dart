5596c9d455331b0cb714b84b9b883a45ae41a514
HirooHG <hugo.golliet@etu.univ-smb.fr>
2023-03-05 21:53:16 +0100

    changing url

--- test/main_test.dart
+++ test/main_test.dart
@@ -5,7 +5,7 @@ import 'package:test/test.dart';
 void main() {
   test('connect', () async {
     var socket = await WebSocket.connect(
-      "ws://localhost:3402/ws"
+      "ws://hugogolliet.fr:34001/ws"
     );
 
     socket.add("heya boi");
